3558d94fc6709937ae7874b30363a38744c7a621
ethical0andrew <frintu.andrei07@gmail.com>
2022-01-07 21:11:33 +0200

    385

--- pbinfo-251/main.cpp
+++ pbinfo-251/main.cpp
@@ -1,22 +1,18 @@
 #include <fstream>
 using namespace std;
-ifstream in("interclasare3.in");
-ofstream out("interclasare3.in");
+ifstream in("interclasare2.in");
+ofstream out("interclasare2.out");
 int main() {
     int a[100001], b[100001], c[200001], n, m, k, i, j;
-    in >> n >> m; for (int i = 1; i <= n; i++) in >> a[i];
-    for (int j = 1; j <= m; j++) in >> b[j];
+    in >> n; for (int i = 1; i <= n; i++) in >> a[i];
+    in >> m; for (int j = 1; j <= m; j++) in >> b[j];
     i = 1; j = 1; k = 0;
     while (i <= n && j <= m) {
-        if (a[i] % 2 == 0) {
-            if (b[j] % 2 == 0) {
-                if (a[i] < b[j]) c[++k] = a[i++];
-                else c[++k] = b[j++];
-            } else j++; } else i++; 
-    } while (i <= n) if (a[i] % 2 == 0) c[++k] = a[i++];
-        else i++;
-    while (j <= m) {
-        if (b[j] % 2 == 0) c[++k] = b[j++];
+        if (a[i] == b[j]) c[++k] = a[i++], j++;
+        else if (a[i] < b[j]) i++;
         else j++;
+    } for (int t = 1; t <= k; t++) {
+        out << c[t] << " ";
+        if (t % 10 == 0) out << endl;
     } return 0;
 }
